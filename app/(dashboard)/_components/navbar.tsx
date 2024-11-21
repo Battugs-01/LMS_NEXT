a4582548c4463d14b08db5294f0f108b4b814a9a
tugsking2002 <tugsking2002@gmail.com>
2024-11-21 08:47:55 +0800

    [devs]

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -6,6 +6,7 @@ const Navbar = () => {
     <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
       <MobileSideBar />
       <NavBarRoutes />
+      <div>xxas</div>
     </div>
   );
 };
